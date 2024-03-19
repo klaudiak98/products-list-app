e3238fea221eeda3510b7c1c53a926bcadc73834
Klaudia Kieryk <klaudiakieryk123@gmail.com>
2024-03-19 15:24:50 +0100

    Prepare basic theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Typography, Container } from '@mui/material'
 import './App.css'
 
 function App() {
 
   return (
-    <p>Hello World!</p>
+    <Container sx={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <Typography variant='h1' color='primary.main'>Hello World</Typography>
+    </Container>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider, createTheme } from '@mui/material'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#61dab3'
+    }
+  },
+  typography: {
+    h1: {
+      fontSize: "3rem",
+      fontWeight: 600
+    },
+    h2: {
+      fontSize: "1.75rem",
+      fontWeight: 400
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
