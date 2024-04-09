9767811d41618f9e892928d94ffad175230752dc
Klaudia Kieryk <klaudiakieryk123@gmail.com>
2024-04-09 17:31:36 +0200

    Separate fetch functions and use debounce

---
+++ src/components/ProductFilter.tsx
@@ -0,0 +1,68 @@
+import TextField from "@mui/material/TextField"
+import { ChangeEvent } from "react"
+import { Product } from "../utils/productType"
+import { ErrorType } from "../utils/ErrorType"
+import { NavigateFunction, useNavigate } from "react-router-dom"
+import { useState } from "react"
+import { debounce } from "@mui/material"
+import { fetchProductById } from "../utils/fetchProductById"
+import { fetchProductsByPage } from "../utils/fetchProductsByPage"
+
+const handleFetchProduct = async (
+  setLoading: (loading: boolean) => void,
+  setProductsList: (products: Product[]) => void,
+  setError: (error: ErrorType | null) => void, 
+  productId: number, 
+  page: number, 
+  navigate: NavigateFunction
+  ) => {
+  if (productId) {
+    await fetchProductById(productId, setProductsList, setError, setLoading)
+    navigate (`?page=${page+1}&id=${productId}`)
+  } else {
+    await fetchProductsByPage(page, setProductsList, setError, setLoading)
+    navigate (`?page=${page+1}`)
+  }
+}
+
+const debounceFetchingProduct = debounce(handleFetchProduct, 500)
+
+const ProductFilter = ({ 
+  productId, 
+  page,
+  setLoading,
+  setProductsList,
+  setError
+}: {
+  productId: number, 
+  page: number,
+  setLoading: (loading: boolean) => void,
+  setProductsList: (products: Product[]) => void,
+  setError: (error: ErrorType | null) => void
+}) => {
+
+  const navigate = useNavigate();
+
+  const [selectedProduct, setSelectedProduct] = useState<number>(productId)
+
+  const handleChooseId = (choosenId: number) => {
+    setSelectedProduct(choosenId)
+  }
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value: number = Number(e.target.value)
+    handleChooseId(value)
+    debounceFetchingProduct(setLoading, setProductsList, setError, value, page, navigate)
+  }
+
+  return (
+    <TextField 
+        type='number' 
+        label='Choose ID' 
+        value={selectedProduct || ''} 
+        onChange={handleOnChange} 
+        inputProps={{min: 1}}/>
+  )
+}
+
+export default ProductFilter
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,15 @@
-import { ChangeEvent, MouseEvent, useEffect, useState } from "react"
-import axios, { AxiosError } from "axios"
+import { MouseEvent, useEffect, useState } from "react"
 import { Product } from "../utils/productType"
 import ProductsTable from "../components/ProductsTable"
 import ErrorMessage from "../components/ErrorMessage"
 import ProductDetails from "../components/ProductDetails"
 import { useNavigate, useLocation } from 'react-router-dom';
 import { SelectedProductProvider } from '../context/SelectedProductContext';
-import { ROWS_PER_PAGE } from "../data/tableData"
 import { ErrorType } from "../utils/ErrorType"
 import Header from "../components/Header"
-import ProductSelector from "../components/ProductSelector"
+import ProductFilter from "../components/ProductFilter"
+import { fetchProductById } from "../utils/fetchProductById"
+import { fetchProductsByPage } from "../utils/fetchProductsByPage"
 
 const Home = () => {
     const navigate = useNavigate();
@@ -18,63 +18,36 @@ const Home = () => {
     const pageParam: string = searchParams.get('page') || ''
     const idParam: string = searchParams.get('id') || ''
 
-    const [productID, setProductID] = useState<number>(parseInt(idParam) || 0)
+    const productId: number = (parseInt(idParam) || 0)
+    const [page, setPage] = useState<number>(parseInt(pageParam)-1 || 0)
+
     const [productsList, setProductsList] = useState<Product[]>([])
     const [loading, setLoading] = useState<boolean>(false)
-    const [page, setPage] = useState<number>(parseInt(pageParam)-1 || 0)
     const [error, setError] = useState<ErrorType | null>(null)
-
-    const handleChooseID = (e: ChangeEvent<HTMLInputElement>) => {
-        const prodID = Number(e.target.value)
-        setProductID(prodID)
-
-        prodID
-        ? navigate(`?page=${page+1}&id=${prodID}`)
-        : navigate(`?page=${page+1}`)
-    }
-
-    const fetchProducts = async () => {
-        try {
-            if (productID) {
-                const response = await axios.get(`https://reqres.in/api/products?id=${productID}`)
-                setProductsList([response.data.data])
-            } else {
-                const response = await axios.get(`https://reqres.in/api/products?per_page=${ROWS_PER_PAGE}}&page=${page+1}`)
-                setProductsList(response.data.data)
-            }
-            setError(null)
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                const axiosError: AxiosError = error;
-                if (axiosError.response?.status === 404) {
-                    setError({ code: axiosError.response?.status, message: 'Not Found. Please change your parameters.' })
-                } else if (axiosError.response?.status === 500) {
-                    setError({ code: axiosError.response?.status, message: 'Internal Server Error' })
-                }
-            } else {
-                setError({code: 500, message: 'Unknown error'})
-            }
-        }
-        setLoading(false)
-    }
 
     const handleChangePage = (_e: MouseEvent<HTMLButtonElement> | null, newPage: number) => {
         setPage(newPage)
         
-        productID
-        ? navigate (`?page=${newPage+1}&id=${productID}`)
+        productId
+        ? navigate (`?page=${newPage+1}&id=${productId}`)
         : navigate (`?page=${newPage+1}`)
     }
 
     useEffect(() => {
-        setLoading(true)
-        fetchProducts()
-    },[page, productID])
+        productId
+        ? fetchProductById(productId, setProductsList, setError, setLoading)
+        : fetchProductsByPage(page, setProductsList, setError, setLoading)
+    },[page, productId])
 
   return (
     <>
         <Header/>
-        <ProductSelector productId={productID} handleChooseId={handleChooseID}/>
+        <ProductFilter 
+            productId={productId} 
+            page={page}
+            setLoading={setLoading}
+            setProductsList={setProductsList}
+            setError={setError}/>
         
         {!error &&  productsList &&
         <>
